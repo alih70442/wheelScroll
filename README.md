1d35866570ef46f10e7369611f57c23514561b90
Ali H <alih70442@gmail.com>
2021-05-30 09:20:22 +0430

    scroll_y version 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wheelScroll
 wheel scroll, easy to install
+
+
+# how to use
+# browser compatibility
--- dist/js/wheelScroll.js
+++ dist/js/wheelScroll.js
@@ -1,26 +1,33 @@
 
 
 // updates
-// handler reachment back animation
 // dynamic handler height
+// update values
+// options
+// horizanol scroll
+// horizanol & vertical togather
+// auto make
 
 {
     // html selectors
-    let scroll = '.js-wheelScroll';
-    let scroll__outer = '.js-wheelScroll__outer';
-    let scroll__inner = '.js-wheelScroll__inner';
-    let scroll__rail = '.js-wheelScroll__rail'
-    let scroll__handler = '.js-wheelScroll__handler';
+    let scroll__y = '.js-wheelScroll__y';
+    let scroll__outer__y = '.js-wheelScroll__outer__y';
+    let scroll__inner__y = '.js-wheelScroll__inner__y';
+    let scroll__rail__y = '.js-wheelScroll__rail__y'
+    let scroll__handler__y = '.js-wheelScroll__handler__y';
 
     class WheelScroll {
         constructor(selector) {
             // selectors
             this.sc = selector;
-            this.sc_outer = $(this.sc).find(scroll__outer);
-            this.sc_inner = $(this.sc).find(scroll__inner);
-            this.sc_rail = $(this.sc).find(scroll__rail);
-            this.sc_handler = $(this.sc).find(scroll__handler);
+            this.sc_outer;
+            this.sc_inner;
+            this.sc_rail;
+            this.sc_handler;
 
+            this.init_html();
+            this.init_style();
+            this.is_disable();
             this.events();
         }
 
@@ -33,7 +40,7 @@
 
                 mouseenter: function () {
                     // if scroll__inner has overflowed
-                    if ($(this).height() < obj.sc_inner.height()) {
+                    if (obj.is_disable() == false) {
                         obj.sc_rail.addClass('wheelScroll__rail-active');
                         obj.sc_handler.addClass('wheelScroll__handler-active');
                     }
@@ -46,10 +53,10 @@
 
                 // update handler pos by scrolling with mouse wheel
                 scroll: function () {
-                    let sc_percent = ($(this).scrollTop()) / ($(this).get(0).scrollHeight - $(this).innerHeight() + 17);
+                    let sc_percent = $(this).scrollTop() / ($(this).get(0).scrollHeight - $(this).height());
                     obj.sc_handler.css('top', sc_percent * (obj.sc_rail.height() - obj.sc_handler.height()));
                 }
-            })
+            });
 
             // scroll by moving handler
             obj.sc_handler.on('mousedown touchstart', function (e) {
@@ -59,12 +66,12 @@
                 obj.sc_handler.addClass('wheelScroll__handler-run-active');
 
                 // static vars
-                let sc_height = obj.sc_rail.height() - obj.sc_handler.height() - 17;
-                let con_height = obj.sc_inner.height() - obj.sc_outer.height() + 17;
+                let sc_height = obj.sc_rail.height() - obj.sc_handler.height();
+                let con_height = obj.sc_inner.height() - obj.sc_outer.height();
                 let last_top = parseInt($(this).css('top'));
                 let first_touch = e.clientY || e.originalEvent.touches[0].pageY;
 
-                $(document).mousemove(function (event) {
+                $(document).on('mousemove touchmove', function (event) {
 
                     // dynamic vars
                     let now_touch = event.pageY || event.originalEvent.touches[0].pageY;
@@ -73,21 +80,21 @@
                     let sc_top_percent = calc_sc / sc_height;
                     let calc_con_top = sc_top_percent * con_height;
 
-                    if (sc_top_percent >= 0 && sc_top_percent <= 1) {
+                    if (sc_top_percent > 0 && sc_top_percent < 1) {
                         // scroll__handler pos
                         obj.sc_handler.css('top', calc_sc);
                         // scroll__outer scrollTop pos
                         obj.sc_outer.scrollTop(calc_con_top);
 
-                    } else if (sc_top_percent < 0 && sc_top_percent >= -0.8) {
+                    } else if (sc_top_percent <= 0) {
                         // animation for top limitation reachment
-                        obj.sc_handler.css('top', (obj.sc_handler.height() * sc_top_percent));
+                        console.log();
+                        obj.sc_handler.css('top', -Math.sqrt((obj.sc_handler.height() * Math.abs(sc_top_percent))));
 
-                    } else if (sc_top_percent > 1 && sc_top_percent <= 1.8) {
+                    } else if (sc_top_percent >= 1) {
                         // animation for bottom limitation reachment
-                        obj.sc_handler.css('top', (obj.calc_last_top() + obj.sc_handler.height() * (sc_top_percent - 1)));
+                        obj.sc_handler.css('top', obj.calc_last_top() + Math.sqrt((obj.sc_handler.height() * Math.abs(sc_top_percent - 1))));
                     }
-
                 })
             });
 
@@ -104,7 +111,60 @@
                     obj.sc_handler.css('top', (obj.calc_last_top() + 'px'));
                     obj.bottom_limit_reachment = false;
                 }
-            })
+            });
+
+            // scroll by clicking on rail
+            $(obj.sc_rail).click(function (e) {
+                // var x = e.pageX - $(this).offset().left;
+                var mouse_y = e.pageY - $(this).offset().top;
+                var handler_top = $(obj.sc_handler).position().top
+
+                if (handler_top > mouse_y)
+                    obj.sc_outer.scrollTop(obj.sc_outer.scrollTop() - 150);
+                else
+                    obj.sc_outer.scrollTop(obj.sc_outer.scrollTop() + 150);
+            });
+
+
+        }
+
+        init_html() {
+            $(this.sc).wrapInner(`<div class="wheelScroll__outer js-wheelScroll__outer">
+                <div class="wheelScroll__inner js-wheelScroll__inner"></div>
+            <div>`);
+            $(this.sc).find('.js-wheelScroll__outer').prepend(`
+                <div class="wheelScroll__rail wheelScroll__rail-mobile js-wheelScroll__rail"></div>
+                <div class="wheelScroll__handler wheelScroll__handler-mobile js-wheelScroll__handler"></div>
+            `)
+
+            // set pointers
+            this.sc_outer = $(this.sc).find(scroll__outer__y);
+            this.sc_inner = $(this.sc).find(scroll__inner__y);
+            this.sc_rail = $(this.sc).find(scroll__rail__y);
+            this.sc_handler = $(this.sc).find(scroll__handler__y);
+        }
+
+        init_style() {
+            // set init height for handler
+            this.sc_handler.css('height', this.calc_handler_height());
+        }
+
+        is_disable() {
+            // if inner.height < outer.height
+            if (this.sc_outer.height() > this.sc_inner.height()) {
+                this.sc_rail.hide();
+                this.sc_handler.hide();
+                return true;
+            }
+            else {
+                this.sc_rail.show();
+                this.sc_handler.show();
+                return false;
+            }
+        }
+
+        calc_handler_height() {
+            return this.sc_outer.height() * this.sc_rail.height() / this.sc_inner.height();
         }
 
         calc_now_top() {
@@ -117,7 +177,7 @@
     }
 
     // initilize
-    document.querySelectorAll('.js-wheelScroll').forEach(function (item) {
+    document.querySelectorAll('.js-wheelScroll__y').forEach(function (item) {
         new WheelScroll(item);
     })
 }
